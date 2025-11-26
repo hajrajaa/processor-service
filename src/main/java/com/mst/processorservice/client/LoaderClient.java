a6583f7636f895506b3e936f1dfe5cbd98a7c20e
hajrajaa <129968202+hajrajaa@users.noreply.github.com>
2025-11-26 20:45:50 +0200

    added loader client and metric client.

--- src/main/java/com/mst/processorservice/client/LoaderClient.java
+++ src/main/java/com/mst/processorservice/client/LoaderClient.java
@@ -4,7 +4,7 @@ package com.mst.processorservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "metric-service", url = "${services.metric.url}")
+@FeignClient(name = "metric-service", url = "${services.loader.url}")
 public interface LoaderClient {
 
     @PostMapping("/loader/scan/manual")
